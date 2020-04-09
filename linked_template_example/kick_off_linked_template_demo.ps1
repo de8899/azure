568abb2a76f961bc3defa8413c21f3f6d2acb0f5
de8899 <de8899@zoho.com>
2020-04-08 22:39:48 -0700

    adding scripts and json files

---
+++ linked_template_example/kick_off_linked_template_demo.ps1
@@ -0,0 +1,29 @@
+
+$projectName = Read-Host -Prompt "Enter a project name:"   # This name is used to generate names for Azure resources, such as storage account name.
+$templateFile = Read-Host -Prompt "Enter the main template file and path"
+
+$resourceGroupName="${projectName}rg"
+$storageAccountName="${projectName}store"
+$containerName = "templates"
+$fileName = "linkedStorageAccount.json" # A file name used for downloading and uploading the linked template.
+
+$key = (Get-AzStorageAccountKey -ResourceGroupName $resourceGroupName -Name $storageAccountName).Value[0]
+$context = New-AzStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $key
+
+# Generate a SAS token
+$linkedTemplateUri = New-AzStorageBlobSASToken `
+    -Context $context `
+    -Container $containerName `
+    -Blob $fileName `
+    -Permission r `
+    -ExpiryTime (Get-Date).AddHours(2.0) `
+    -FullUri
+
+# Deploy the template
+New-AzResourceGroupDeployment `
+  -Name DeployLinkedTemplate `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile $templateFile `
+  -projectName $projectName `
+  -linkedTemplateUri $linkedTemplateUri `
+  -verbose
