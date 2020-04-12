1e887a34bcc71258bf745ac86509e3257c6b77f3
de8899 <de8899@zoho.com>
2020-04-11 23:34:43 -0700

    updated deploy script header with comments on what changes are needed for each deployment.

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,7 @@
+# Each deployment needs changes in params file to: vmname, networkinterfacename, and newpublicipaddress
 $adminName = Read-Host -Prompt "Enter the Admin name"
 $adminPassword = Read-Host -Prompt "Enter the admin password" -AsSecureString
-New-AzResourceGroupDeployment -Name deployvm2 -ResourceGroupName armtesting1 `
+New-AzResourceGroupDeployment -Name deploy01 -ResourceGroupName armtestRG `
 -adminUsername $adminName `
 -adminPassword $adminPassword `
 -TemplateFile ".\w001template.json" `
