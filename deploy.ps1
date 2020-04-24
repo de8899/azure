7b22302c9f55ed42ef805c0b592935aa8c8546e3
de8899 <de8899@zoho.com>
2020-04-23 22:02:49 -0700

    fixed resource group name issue

--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,7 @@
 # Each deployment needs changes in params file to: vmname, networkinterfacename, and newpublicipaddress
 $adminName = Read-Host -Prompt "Enter the Admin name"
 $adminPassword = Read-Host -Prompt "Enter the admin password" -AsSecureString
-New-AzResourceGroupDeployment -Name deploy01 -ResourceGroupName armtestRG `
+New-AzResourceGroupDeployment -Name deploy01 -ResourceGroupName RG2 `
 -adminUsername $adminName `
 -adminPassword $adminPassword `
 -TemplateFile ".\w001template.json" `
